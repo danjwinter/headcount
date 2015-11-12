731760a71a0536f6aea5944276d5e0d66a0a1fc8
martensonbj <martensonbj@gmail.com>
2015-11-12 14:20:46 -0700

    refactored parser repository

--- lib/parser_repository.rb
+++ lib/parser_repository.rb
@@ -5,27 +5,27 @@ require 'pry'
 class ParserRepository
   attr_reader :enrollment
 
-  # def clean_kindergarten_participation_numbers(arg)
-  #   arg.each do |k, v|
-  #     if v.is_a?(Numeric) == false
-  #       kindergarten_participation[k] = "N/A"
-  #     end
-  #   end
-  # end
-
   def initialize(file_set)
     @enrollment = file_set.fetch(:enrollment)
   end
 
   def parsed
     parsed_category_data = []
+    send_to_kindergarten_enrollment_parser(enrollment, parsed_category_data)
+    send_to_high_school_enrollment_parser(enrollment, parsed_category_data)
+    parsed_category_data
+  end
+
+  def send_to_kindergarten_enrollment_parser(enrollment, parsed_category_data)
     if kindergarten_path(enrollment)
       parsed_category_data << KindergartenEnrollmentParser.new(kindergarten_path(enrollment)).district_data
     end
+  end
+
+  def send_to_high_school_enrollment_parser(enrollment, parsed_category_data)
     if high_school_path(enrollment)
       parsed_category_data << HighSchoolEnrollmentParser.new(high_school_path(enrollment)).district_data
     end
-    parsed_category_data
   end
 
   def high_school_path(category)

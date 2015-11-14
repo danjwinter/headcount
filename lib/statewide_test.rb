496ffde7c52091dda8eb8eff59cec7bb8128453e
Dan Winter <dan.j.winter@gmail.com>
2015-11-13 17:02:59 -0700

    added race to statewide test

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -1,7 +1,7 @@
 class UnknownDataError < StandardError
 end
 
-class UnkownRaceError < StandardError
+class UnknownRaceError < StandardError
 end
 
 class StatewideTest
@@ -10,19 +10,57 @@ class StatewideTest
   def initialize(data)
     @name = data.upcase
     @grade_proficiency = {}
+    @race_stats = {}
   end
 
   def load_new_data(attribute)
+    grab_that_grade_proficiency(attribute)
+    grab_that_race(attribute)
+  end
+
+  def grab_that_grade_proficiency(attribute)
     @grade_proficiency[3] ||= attribute[:third_grade]
     @grade_proficiency[8] ||= attribute[:eighth_grade]
   end
 
+  def grab_that_race(attribute)
+    @race_stats[:asian] ||= attribute[:asian]
+    @race_stats[:black] ||= attribute[:black]
+    @race_stats[:pacific_islander] ||= attribute[:pacific_islander]
+    @race_stats[:hispanic] ||= attribute[:hispanic]
+    @race_stats[:native_american] ||= attribute[:native_american]
+    @race_stats[:white] ||= attribute[:white]
+    @race_stats[:two_or_more] ||= attribute[:two_or_more]
+  end
+
+  def proficient_by_race_or_ethnicity(race)
+    if available_races.include?(race)
+      hash_access(@race_stats[race])
+    else
+      raise UnknownRaceError
+    end
+  end
+
+  def available_races
+    [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
+  end
+
   def proficient_by_grade(number)
     if available_grades.include?(number)
-      @grade_proficiency[number]
+      hash_access(@grade_proficiency[number])
     else
       raise UnknownDataError
     end
+  end
+
+  def hash_access(value)
+    new_value = {}
+    value.each do |key, value|
+      new_value[key] = value.each do |next_key, next_value|
+        value[next_key] = truncate(next_value)
+      end
+    end
+    new_value
   end
 
   def available_grades
--- test/statewide_test_test.rb
+++ test/statewide_test_test.rb
@@ -10,12 +10,36 @@ class StatewideTestTest < Minitest::Test
     @st1 = StatewideTest.new("Colorado")
   end
 
+  def hash_access(value)
+    new_value = {}
+    value.each do |key, value|
+      new_value[key] = value.each do |next_key, next_value|
+        value[next_key] = truncate(next_value)
+      end
+    end
+    new_value
+  end
+
+  def truncate(value)
+    ((value * 1000).floor/1000.0)
+  end
+
   def statewide_data_3
+    {:third_grade => { 2008 => {:math => 0.857, :reading => 0.866, :writing => 0.671},
+     2009 => {:math => 0.8245, :reading => 0.862, :writing => 0.706},
+     2010 => {:math => 0.849, :reading => 0.864, :writing => 0.662},
+     2011 => {:math => 0.819, :reading => 0.867, :writing => 0.678},
+     2012 => {:math => 0.830, :reading => 0.8790, :writing => 0.655},
+     2013 => {:math => 0.855, :reading => 0.859, :writing => 0.668},
+     2014 => {:math => 0.8341, :reading => 0.831, :writing => 0.639}
+   }}
+  end
+  def truncated_statewide_data_3
     {:third_grade => { 2008 => {:math => 0.857, :reading => 0.866, :writing => 0.671},
      2009 => {:math => 0.824, :reading => 0.862, :writing => 0.706},
      2010 => {:math => 0.849, :reading => 0.864, :writing => 0.662},
      2011 => {:math => 0.819, :reading => 0.867, :writing => 0.678},
-     2012 => {:math => 0.830, :reading => 0.870, :writing => 0.655},
+     2012 => {:math => 0.830, :reading => 0.879, :writing => 0.655},
      2013 => {:math => 0.855, :reading => 0.859, :writing => 0.668},
      2014 => {:math => 0.834, :reading => 0.831, :writing => 0.639}
    }}
@@ -26,10 +50,26 @@ class StatewideTestTest < Minitest::Test
      2009 => {:math => 0.824, :reading => 0.862, :writing => 0.706},
      2010 => {:math => 0.849, :reading => 0.864, :writing => 0.662},
      2011 => {:math => 0.819, :reading => 0.867, :writing => 0.678},
-     2012 => {:math => 0.830, :reading => 0.870, :writing => 0.655},
+     2012 => {:math => 0.830, :reading => 0.587, :writing => 0.655},
      2013 => {:math => 0.855, :reading => 0.859, :writing => 0.668},
      2014 => {:math => 0.834, :reading => 0.831, :writing => 0.639}
      }}
+  end
+
+  def asian_data
+    {:asian => { 2011 => {math: 0.816, reading: 0.897, writing: 0.8826},
+     2012 => {math: 0.818, reading: 0.893, writing: 0.808},
+     2013 => {math: 0.8085, reading: 0.901, writing: 0.810},
+     2014 => {math: 0.800, reading: 0.8855, writing: 0.789},
+   }}
+  end
+
+  def pacific_islander_data
+   {:pacific_islander => { 2011 => {math: 0.816, reading: 0.897, writing: 0.826},
+    2012 => {math: 0.818, reading: 0.893, writing: 0.808},
+    2013 => {math: 0.805, reading: 0.901, writing: 0.810},
+    2014 => {math: 0.800, reading: 0.855, writing: 0.789},
+  }}
   end
 
   def test_class_exists
@@ -46,12 +86,12 @@ class StatewideTestTest < Minitest::Test
 
   def test_it_can_retrieve_third_grade_data
     @st.load_new_data(statewide_data_3)
-    assert_equal statewide_data_3.fetch(:third_grade),@st.proficient_by_grade(3)
+    assert_equal truncated_statewide_data_3.fetch(:third_grade), @st.proficient_by_grade(3)
   end
 
   def test_it_can_retrieve_eighth_grade_data
     @st.load_new_data(statewide_data_8)
-    assert_equal statewide_data_8.fetch(:eighth_grade),@st.proficient_by_grade(8)
+    assert_equal statewide_data_8.fetch(:eighth_grade), @st.proficient_by_grade(8)
   end
 
   def test_it_raises_an_error_with_bad_grade
@@ -60,6 +100,24 @@ class StatewideTestTest < Minitest::Test
       @st.proficient_by_grade(5)
     end
   end
+
+  def test_it_can_find_data_based_on_race
+    @st.load_new_data(asian_data)
+    assert_equal hash_access(asian_data.fetch(:asian)), @st.proficient_by_race_or_ethnicity(:asian)
+  end
+
+  def test_it_raises_an_error_with_unavailable_data
+    @st.load_new_data(asian_data)
+    assert_raises UnknownRaceError do
+      @st.proficient_by_race_or_ethnicity(:canuck)
+    end
+  end
+
+  def test_it_can_find_data_based_on_a_different_race
+    @st.load_new_data(pacific_islander_data)
+    assert_equal hash_access(pacific_islander_data.fetch(:pacific_islander)), @st.proficient_by_race_or_ethnicity(:pacific_islander)
+  end
+
 
 
 end

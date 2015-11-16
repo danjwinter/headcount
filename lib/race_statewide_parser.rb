a031b4942eadc72f79f6cd2dc99293c087c75543
Dan Winter <dan.j.winter@gmail.com>
2015-11-15 18:39:52 -0700

    refactored statewide race parser and added tests

--- lib/race_statewide_parser.rb
+++ lib/race_statewide_parser.rb
@@ -13,12 +13,10 @@ class RaceStatewideParser
   def load_info(path_subject)
     @csv = CSV.read(path_subject[0], {headers: true, header_converters: :symbol}).map {|row| row.to_h}
     @subject = path_subject[1]
-    # binding.pry
     build_data_set
   end
 
   def build_data_set
-    # binding.pry
     if self.data_set.keys.empty?
       set_up_data_hash
     end
@@ -30,16 +28,11 @@ class RaceStatewideParser
   end
 
   def set_up_data_hash
-    # binding.pry
-
     districts = grouped_data_by_district_name.keys.each {|district| district.upcase}
     races = []
     dates = []
     collect_races_and_dates(races, dates)
     configure_hash(districts, races, dates)
-    # districts.each do |district|
-    #   @data_set[district.upcase] = race_setup(races.uniq, dates.uniq)
-    # end
   end
 
   def configure_hash(districts, races, dates)
@@ -51,7 +44,6 @@ class RaceStatewideParser
   def collect_races_and_dates(races, dates)
     grouped_data_by_district_name.each do |key, value|
       value.each do |line|
-        # binding.pry
         races << symbolize_spaced(line[:race_ethnicity])
         dates << line[:timeframe].to_i
       end
@@ -88,36 +80,10 @@ class RaceStatewideParser
     value.each do |line|
       district = key.upcase
       race = symbolize_spaced(line[:race_ethnicity])
-
       year = line[:timeframe].to_i
-      @subject
       score = line[:data].to_f
       self.data_set[district][race][year][@subject] = score
     end
-
-    districts_data_collection = {}
-    year_data = {}
-    districts_data_collection[:name] = key.upcase
-    districts_data_collection[@race] = year_prep(value, year_data)
-    final_data[key] = districts_data_collection
-  end
-
-  def year_prep(attributes, year_data)
-    attributes.map do |attribute|
-      attribute[:timeframe].to_i
-    end.uniq.each do |year|
-      year_data[year] = {}
-    end
-    set_subject_and_score(year_data, attributes)
-  end
-
-  def set_subject_and_score(year_data, attributes)
-    attributes.each do |attribute|
-      race = symbolize_spaced(attribute[:race_ethnicity])
-      score = attribute[:data].to_f
-      year_data[attribute[:timeframe].to_i].merge!({race => score})
-    end
-    year_data
   end
 
   def grouped_data_by_district_name
@@ -126,9 +92,4 @@ class RaceStatewideParser
     end
   end
 
-  def enrollment_data
-    @csv.group_by do |row|
-      row[:location]
-    end
-  end
 end
--- test/race_statewide_parser_test.rb
+++ test/race_statewide_parser_test.rb
@@ -77,40 +77,52 @@ class RaceStatewideParserTest < Minitest::Test
     assert_equal collected_dates, dates.uniq
   end
 
-  def test_something
-
-    @rsp.load_info([file_set_math_statewide, :math])
+  def test_date_setup_sets_up_dates
+    expected_date_setup = {1969 => {math: nil, reading: nil, writing: nil},
+                           1412 => {math: nil, reading: nil, writing: nil}}
+    assert_equal expected_date_setup,@rsp.date_setup([1969, 1412])
   end
 
-
-  def test_load_info_retrieves_asian_data
-    skip
+  def test_load_info_retrieves_asian_math_data
     @rsp.load_info([file_set_math_statewide, :math])
     @rsp.load_info([file_set_reading_statewide, :reading])
     @rsp.load_info([file_set_writing_statewide, :writing])
 
-    assert_equal asian_opts, @rsp.proficient_by_race_or_ethnicity(:asian)
+    assert_equal 0.8169, @rsp.data_set["ACADEMY 20"][:asian][2011][:math]
+    assert_equal 0.8182, @rsp.data_set["ACADEMY 20"][:asian][2012][:math]
   end
 
+  def test_load_info_retrieves_black_math_data
+    @rsp.load_info([file_set_math_statewide, :math])
+    @rsp.load_info([file_set_reading_statewide, :reading])
+    @rsp.load_info([file_set_writing_statewide, :writing])
 
-  def test_parser_creates_array_of_one_hash_upon_initialization
-    skip
-    assert_equal Hash, @rsp.csv[0].class
+    assert_equal 0.2252, @rsp.data_set["ADAMS COUNTY 14"][:black][2012][:math]
+    assert_equal 0.1961, @rsp.data_set["ADAMS COUNTY 14"][:black][2011][:math]
+  end
+
+  def test_load_info_retrieves_hispanic_reading_data
+    @rsp.load_info([file_set_math_statewide, :math])
+    @rsp.load_info([file_set_reading_statewide, :reading])
+    @rsp.load_info([file_set_writing_statewide, :writing])
+
+    assert_equal 0.41874, @rsp.data_set["ADAMS COUNTY 14"][:hispanic][2012][:reading]
+    assert_equal 0.4341, @rsp.data_set["ADAMS COUNTY 14"][:hispanic][2011][:reading]
+  end
+
+  def test_load_info_retrieves_pacific_islander_writing_data
+    @rsp.load_info([file_set_math_statewide, :math])
+    @rsp.load_info([file_set_reading_statewide, :reading])
+    @rsp.load_info([file_set_writing_statewide, :writing])
+
+    assert_equal 0.3564, @rsp.data_set["ADAMS-ARAPAHOE 28J"][:pacific_islander][2012][:writing]
+    assert_equal 0.3529, @rsp.data_set["ADAMS-ARAPAHOE 28J"][:pacific_islander][2011][:writing]
   end
 
   def test_district_data_creates_keys_based_on_locations
-    skip
-    locations = ["Colorado", "ACADEMY 20", "ADAMS COUNTY 14", "ADAMS-ARAPAHOE 28J"]
-    assert_equal locations, @rsp.district_data.keys
-  end
-
-  def test_district_data_prints_value
-    skip
-    stats1 = ({2008=> {:math => 0.469, :reading => 0.703, :writing => 0.529},
-    2009=>{:math => 0.499, :reading => 0.726, :writing => 0.528},
-    2010=>{:math => 0.51, :reading => 0.679, :writing => 0.549}})
-    assert_equal({:name=>"COLORADO", :eighth_grade => stats1}, @rsp.district_data.first[1])
-    # assert_equal academy_20_parsed_district_data, @rsp.district_data.fetch("ACADEMY 20")
+      @rsp.load_info([file_set_math_statewide, :math])
+    locations = ["COLORADO", "ACADEMY 20", "ADAMS COUNTY 14", "ADAMS-ARAPAHOE 28J"]
+    assert_equal locations, @rsp.data_set.keys
   end
 
 end

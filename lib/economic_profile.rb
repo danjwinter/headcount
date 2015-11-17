c677e32da7ba86d005d814881aeec67f5d9c6d3f
Dan Winter <dan.j.winter@gmail.com>
2015-11-17 14:37:44 -0700

    set up basic class structure for economic profile

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,5 +1,7 @@
 class EconomicProfile
 
+  attr_reader :median_household_income, :children_in_poverty, :free_or_reduced_price_lunch, :title_i
+
   def initialize(data)
     @median_household_income ||= data[:median_household_income]
     @children_in_poverty ||= data[:children_in_poverty]
--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -18,8 +18,28 @@ class EconomicProfileTest < Minitest::Test
     assert EconomicProfile
   end
 
+  def setup
+    @ep = EconomicProfile.new(data)
+  end
+
   def test_ec_prof_takes_data_on_instantiation
     assert EconomicProfile.new(data)
+  end
+
+  def test_median_household_income_can_be_accessed
+    assert_equal({2015 => 50000, 2014 => 60000}, @ep.median_household_income)
+  end
+
+  def test_children_in_poverty_can_be_accessed
+    assert_equal({2012 => 0.1845}, @ep.children_in_poverty)
+  end
+
+  def test_free_or_reduced_price_lunch_can_be_accessed
+    assert_equal({2014 => {:percentage => 0.023, :total => 100}}, @ep.free_or_reduced_price_lunch)
+  end
+
+  def test_title_i_can_be_accessed
+    assert_equal({2015 => 0.543}, @ep.title_i)
   end
 
 
